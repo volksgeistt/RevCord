8d96e1c40f8109654463e9b9b8a06c115ddbfb2f
Volksgeist <131983323+volksgeistt@users.noreply.github.com>
2024-01-03 15:34:55 +0530

    Create start.bat

---
+++ start.bat
@@ -0,0 +1,8 @@
+pip install discord.py==1.7.3
+pip install discord.py-self==1.9.2
+pip install requests
+pip install colorama
+pip install aiohttp
+pip install pyfiglet
+pip install pyshorteners
+python revcord.py
